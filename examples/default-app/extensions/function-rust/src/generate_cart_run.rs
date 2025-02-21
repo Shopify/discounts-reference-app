d7ead7ea318edf7bee9d819b30a00131d8e8a807
Jonathan Hamel <jonathan.hamel@shopify.com>
2025-02-21 09:27:01 -0500

    modify functions

---
+++ examples/default-app/extensions/function-rust/src/generate_cart_run.rs
@@ -0,0 +1,61 @@
+// [START discount-function.run.cart]
+use shopify_function::prelude::*;
+use shopify_function::Result;
+
+use cart_run::output::{
+    CartLineTarget, CartOperation, FunctionCartRunResult, OrderDiscountCandidate,
+    OrderDiscountCandidateTarget, OrderDiscountCandidateValue, OrderDiscountSelectionStrategy,
+    OrderDiscounts, OrderSubtotalTarget, Percentage, ProductDiscountCandidate,
+    ProductDiscountCandidateTarget, ProductDiscountCandidateValue,
+    ProductDiscountSelectionStrategy, ProductDiscounts,
+};
+
+use cart_run::input::ResponseData;
+
+#[shopify_function_target(
+    target = "cart_run",
+    query_path = "src/generate_cart_run.graphql",
+    schema_path = "schema.graphql"
+)]
+fn generate_cart_run(input: ResponseData) -> Result<FunctionCartRunResult> {
+    let cart_lines = input.cart.lines.iter();
+    // [START discount-function.run.cart.output]
+    Ok(FunctionCartRunResult {
+        operations: vec![
+            CartOperation::AddOrderDiscounts(OrderDiscounts {
+                selection_strategy: OrderDiscountSelectionStrategy::FIRST,
+                candidates: vec![OrderDiscountCandidate {
+                    targets: vec![OrderDiscountCandidateTarget::OrderSubtotal(
+                        OrderSubtotalTarget {
+                            excluded_variant_ids: vec![],
+                        },
+                    )],
+                    message: Some("10% OFF ORDER".to_string()),
+                    value: OrderDiscountCandidateValue::Percentage(Percentage {
+                        value: Decimal(10.0),
+                    }),
+                    conditions: None,
+                    associated_discount_code: None,
+                }],
+            }),
+            CartOperation::AddProductDiscounts(ProductDiscounts {
+                selection_strategy: ProductDiscountSelectionStrategy::FIRST,
+                candidates: cart_lines
+                    .map(|cart_line| ProductDiscountCandidate {
+                        targets: vec![ProductDiscountCandidateTarget::CartLine(CartLineTarget {
+                            id: cart_line.id.clone(),
+                            quantity: None,
+                        })],
+                        message: Some("20% OFF PRODUCT".to_string()),
+                        value: ProductDiscountCandidateValue::Percentage(Percentage {
+                            value: Decimal(20.0),
+                        }),
+                        associated_discount_code: None,
+                    })
+                    .collect(),
+            }),
+        ],
+    })
+    // [END discount-function.run.cart.output]
+}
+// [END discount-function.run.cart]
